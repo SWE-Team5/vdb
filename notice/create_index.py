c48aed7974470627fe303c0693a71a37264d7a1e
Minho Baek <hoeo159@gmail.com>
2024-10-30 01:29:59 +0900

    fix create_index

--- notice/create_index.py
+++ notice/create_index.py
@@ -1,16 +1,18 @@
 from pinecone import Pinecone, ServerlessSpec
 
-pc = Pinecone(api_key="1734fc56-9964-4232-a412-50e211980310")
+def update_index(index_name, dimension, metric):
+    pc = Pinecone(api_key="1734fc56-9964-4232-a412-50e211980310")
 
-index_name = "skku-notice"
+    pc.delete_index(index_name)
 
-pc.create_index(
-    name=index_name,
-    #klue/bert-base
-    dimension=768,
-    metric="cosine",
-    spec=ServerlessSpec(
-        cloud="aws",
-        region="us-east-1"
+    pc.create_index(
+        name=index_name,
+        dimension=dimension,
+        metric=metric,
+        spec=ServerlessSpec(
+            cloud="aws",
+            region="us-east-1"
+        )
     )
-)
+
+update_index("skku-notice", 384, "cosine")
